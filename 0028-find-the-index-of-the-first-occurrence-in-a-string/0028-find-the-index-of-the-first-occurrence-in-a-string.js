fa2abb876c564b70ae56cad87acc4e1ec6908d61
RydKrm <124508907+RydKrm@users.noreply.github.com>
2023-09-23 00:33:00 +0600

    Time: 51 ms (61.71%), Space: 41.6 MB (84.45%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * Solution #1 - Built-in function
+ *
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    return haystack.indexOf(needle);
+};
