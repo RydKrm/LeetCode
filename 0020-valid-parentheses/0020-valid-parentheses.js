91a15b83cd20c93f4af32c4a0228bad547abd945
RydKrm <124508907+RydKrm@users.noreply.github.com>
2023-09-25 00:08:37 +0600

    Time: 56 ms (55.67%), Space: 42.1 MB (73.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i]);
+            count++;
+        } else {
+            if (stack.length === 0) return false;
+            const ch = stack.pop();
+            if ((s[i] === ')' && ch === '(') || (s[i] === ']' && ch === '[') || (s[i] === '}' && ch === '{')) {
+            } else {
+                return false;
+            }
+            count--;
+        }
+    }
+    return count === 0;
+};
